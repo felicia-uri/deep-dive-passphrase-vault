b16b91419b7e50b7f7217b00c17b841651762fcd
felicia-uri <felicia.uri@gmail.com>
2017-10-11 12:55:54 -0600

    Merged and Updated Constants.

--- src/main/java/edu/cnm/deepdive/passphrase/Options.java
+++ src/main/java/edu/cnm/deepdive/passphrase/Options.java
@@ -1,52 +1,122 @@
 package edu.cnm.deepdive.passphrase;
 
 
-import edu.cnm.deepdive.passphrase.util.Constants;
-import edu.cnm.deepdive.passphrase.util.UsageStrings;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
+
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingArgumentException;
 import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.UnrecognizedOptionException;
 
+import edu.cnm.deepdive.passphrase.util.Constants;
+import edu.cnm.deepdive.passphrase.util.UsageStrings;
+
 public class Options {
 
-  public static void main(String[] args) {
+  public final Map<String, Object> map;
+//  public Map<String, Object> getMap() {
+//    return map;
+//  }
+
+  public Options(String[] args)
+      throws ParseException, HelpRequestedException, IllegalArgumentException {
+    ResourceBundle resourceBundle = UsageStrings.getBundle();
+    org.apache.commons.cli.Options options = null;
     try {
-      org.apache.commons.cli.Options options = buildOptions();
-      CommandLineParser parser = new DefaultParser();
-      org.apache.commons.cli.CommandLine cmdLine = parser.parse(options, args);
+      options = buildOptions();
+      map = parse(args, options);
+      validate();
 
-      if(cmdLine.hasOption("l")) {
-        System.out.println(Constants.LENGTH_OPTION);
-      }
 
-      if(cmdLine.hasOption("d")) {
-        System.out.println(Constants.DELIMITER_OPTION);
-      }
-
-      if(cmdLine.hasOption("?")) {
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("java " + Options.class.getName(), options);
-      }
-      if(cmdLine.hasOption("")) {
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("java " + Options.class.getName(), options);
-      }
     } catch (UnrecognizedOptionException e) {
-      System.out.println("Retry: \nPlease type \"-?\" or \"--help\" for command list!");
+      System.out.println(resourceBundle.getString(Constants.UNRECOGNIZED_OPTION));
+      throw e;
     } catch (MissingOptionException e) {
-      System.out.println("Please type \"-?\" or \"--help\" for command list!");
+      // Use this in error msg (Option) e.getMissingOptions().get(0)
+      System.out.println(resourceBundle.getString(Constants.MISSING_OPTION));
+      throw e;
+    } catch (MissingArgumentException e) {
+      System.out.println(resourceBundle.getString(Constants.ARGUMENT_ERROR));// Print out error message from usage strings
+      // Use this in error msg e.getOption().getLongOpt()
+      throw e;
     } catch (ParseException e) {
-      e.printStackTrace();
+      throw e;
+    } catch (HelpRequestedException e) {
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp("java " + Options.class.getName(), options);
+      throw e;
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+      throw e;
     }
   }
 
+  private void validate()
+      throws IllegalArgumentException {
+    ResourceBundle resourceBundle = UsageStrings.getBundle();
+    if(map.containsKey(Constants.PASSWORD_MODE_OPTION)) {
+      System.out.println(Constants.PASSWORD_MODE_OPTION);
+      // check validity of password options
+      if (map.containsKey(Constants.EXCLUDES_UPPERCASE)
+          && map.containsKey(Constants.EXCLUDES_LOWERCASE)
+          && map.containsKey(Constants.EXCLUDES_DIGITS)
+          && map.containsKey(Constants.EXCLUDES_SYMBOLS)) { // confirm with team that this is ok (symbols?)
+        throw new IllegalArgumentException(String.format(resourceBundle.getString(Constants.ARGUMENT_ERROR)));
+      }
+
+      int length = ((Number) map.get(Constants.LENGTH_OPTION)).intValue();
+      if ((length < Constants.MINIMUM_PASSWORD_LENGTH) || (length > Constants.MAXIMUM_PASSWORD_LENGTH)) { // use a constant instead of "12"
+        throw new IllegalArgumentException(String.format(Constants.LENGTH_ERROR));
+      }
+      if (map.containsKey(Constants.DELIMITER_OPTION)) { // No exception thrown warning for Delimiter option
+        System.out.printf(resourceBundle.getString(Constants.DELIMITER_ERROR),
+            Arrays.toString(Constants.DELIMITER_SELECTION));
+      }
+      // Check "no-order" option, throw warning
+    } else {
+      // TODO check delimiter exception
+      if(map.containsKey(Constants.EXCLUDES_ORDER)) {
+        System.out.println(Constants.MODE_SELECT_ERROR);
+      }
+      int length = ((Number) map.get(Constants.LENGTH_OPTION)).intValue();
+      if ((length < Constants.MINIMUM_PASSPHRASE_LENGTH) || (length > Constants.MAXIMUM_PASSPHRASE_LENGTH)) { // use a constant instead of "6"
+        throw new IllegalArgumentException(String.format(Constants.LENGTH_ERROR));
+
+      }
+    }
+  }
+
+  private Map parse(String[] args, org.apache.commons.cli.Options options)
+      throws ParseException, HelpRequestedException {
+    CommandLineParser parser = new DefaultParser();
+    org.apache.commons.cli.CommandLine cmdLine = parser.parse(options, args);
+    ResourceBundle resourceBundle = UsageStrings.getBundle();
+
+    if(cmdLine.hasOption(resourceBundle.getString(Constants.HELP_MSG))) {
+      throw new HelpRequestedException();
+    }
+    Map<String, Object> map = new HashMap<>(); // map is a field(lookup table)
+    for (Option option : cmdLine.getOptions()) {
+      if (option.hasArg()) {
+        Object argValue = cmdLine.getParsedOptionValue(option.getLongOpt());
+        map.put(option.getLongOpt(), argValue);
+      } else {
+        map.put(option.getLongOpt(), null);
+      }
+    }
+    return map;
+  }
+
   // defines my options
-  private static org.apache.commons.cli.Options buildOptions() {
+  private org.apache.commons.cli.Options buildOptions() {
     //returns a builder      //this list of options can be in any order I want
     ResourceBundle bundle = UsageStrings.getBundle();
     Option repeatOpt =         Option.builder("r").hasArg(false)
@@ -54,25 +124,24 @@ public class Options {
                                                        .longOpt(Constants.NO_REPEAT_OPTION)
                                                        .desc(bundle.getString(Constants.EXCLUDES_REPEAT))
                                                        .build();
-    Option uppercaseOpt =      Option.builder("u").hasArg(false)
+    Option uppercaseOpt =       Option.builder("u").hasArg(false)
                                                        .required(false)
                                                        .longOpt(Constants.NO_UPPER_OPTION)
                                                        .desc(bundle.getString(Constants.EXCLUDES_UPPERCASE))
                                                        .build();
-    Option lowercaseOpt =      Option.builder("w").hasArg(false)
+    Option lowercaseOpt =       Option.builder("w").hasArg(false)
                                                        .required(false)
                                                        .longOpt(Constants.NO_LOWER_OPTION)
                                                        .desc(bundle.getString(Constants.EXCLUDES_LOWERCASE))
                                                        .build();
-    Option digitsOpt =         Option.builder("g").hasArg(false)
+    Option digitsOpt =          Option.builder("g").hasArg(false)
                                                        .required(false)
                                                        .longOpt(Constants.NO_DIGITS_OPTION)
                                                        .desc(bundle.getString(Constants.EXCLUDES_DIGITS))
                                                        .build();
-    Option ambiguousOpt =      Option.builder("a").hasArg(false)
+    Option ambiguousOpt =       Option.builder("a").hasArg(false)
                                                        .required(false)
-                                                       .longOpt(
-                                                           Constants.NO_AMBIGUOUS_CHARACTERS_OPTION)
+                                                       .longOpt(Constants.NO_AMBIGUOUS_CHARACTERS_OPTION)
                                                        .desc(bundle.getString(Constants.EXCLUDES_AMBIGUOUS))
                                                        .build();
     Option orderOpt =          Option.builder()        .hasArg(false)
@@ -80,7 +149,7 @@ public class Options {
                                                        .longOpt(Constants.EXCLUDES_ORDER_OPTION)
                                                        .desc(bundle.getString(Constants.EXCLUDES_ORDER))
                                                        .build();
-    Option symbolsOpt =        Option.builder("s").hasArg(true)
+    Option symbolsOpt =         Option.builder("s").hasArg(true)
                                                        .required(false)
                                                        .optionalArg(true)
                                                        .numberOfArgs(1)
@@ -88,7 +157,7 @@ public class Options {
                                                        .desc(bundle.getString(Constants.EXCLUDES_SYMBOLS))
                                                        .type(String.class)
                                                        .build();
-    Option lengthOpt =         Option.builder("l").argName("value") // set its arg name
+    Option lengthOpt =         Option.builder("l").argName("value") // TODO set its arg name
                                                        .optionalArg(false) // mark the builder object as an optional option
                                                        .hasArg(true)
                                                        .numberOfArgs(1)
@@ -109,7 +178,7 @@ public class Options {
     Option helpOpt =           Option.builder("?").longOpt(Constants.HELP_OPTION)
                                                        .required(false)
                                                        .hasArg(false)
-                                                       .desc(Constants.HELP_MSG)
+                                                       .desc(bundle.getString(Constants.HELP_MSG))
                                                        .build();
     Option modeOpt =           Option.builder("x").hasArg(false)
                                                        .required(false)
@@ -132,4 +201,26 @@ public class Options {
     return options;
   }
 
+  public static class HelpRequestedException extends Exception {
+
+    public HelpRequestedException() {
+    }
+
+    public HelpRequestedException(String message) {
+      super(message);
+    }
+
+    public HelpRequestedException(String message, Throwable cause) {
+      super(message, cause);
+    }
+
+    public HelpRequestedException(Throwable cause) {
+      super(cause);
+    }
+
+    public HelpRequestedException(String message, Throwable cause, boolean enableSuppression,
+        boolean writableStackTrace) {
+      super(message, cause, enableSuppression, writableStackTrace);
+    }
+  }
 }
--- src/main/java/edu/cnm/deepdive/passphrase/util/Constants.java
+++ src/main/java/edu/cnm/deepdive/passphrase/util/Constants.java
@@ -1,77 +1,93 @@
 package edu.cnm.deepdive.passphrase.util;
 
-// e.g. keys for accessing resource bundle properties,resources
+//keys for accessing resource bundle properties, resources
 
 public class Constants {
-    // Resource bundles names
-    public static final String USAGE_BUNDLE = "usage-string";
-	public static final String WORDS_BUNDLE = "diceware-words";
+
+  //Resource bundle name
+  public static final String USAGE_BUNDLE = "usage-strings";
+  public static final String WORDS_BUNDLE = "diceware-words";
 
 
-    // Definitions of the Exclusion string variables
-    public static final String EXCLUDES_REPEAT = "excludesRepeat";
-    public static final String EXCLUDES_UPPERCASE = "excludesUppercase";
-    public static final String EXCLUDES_LOWERCASE = "excludesLowercase";
-    public static final String EXCLUDES_DIGITS = "excludesDigits";
-    public static final String EXCLUDES_SYMBOLS = "excludesSymbols";
-    public static final String EXCLUDES_AMBIGUOUS = "excludesAmbiguous";
-    public static final String EXCLUDES_ORDER = "excludesOrder";
-
-    // Definitions of the Specifications string variables
-    public static final String SPECIFY_LENGTH = "specifyLength";
-    public static final String SPECIFY_DELIMITER = "specifyDelimiter";
-
-    // Definition of the mode switch string
-    public static final String MODE_SWITCH= "modeSwitch";
-
-    // Definition of the Help Message string
-    public static final String HELP_MSG = "helpMsg";
-
-    // Definitions of the Error Messages
-    public static final String MODE_SELECT_ERROR = "modeSelectError";
-    public static final String LENGTH_ERROR = "lengthError";
-
-    // Definitions of the Warning Messages
-    public static final String LENGTH_WARNING = "lengthWarning";
-    public static final String MODE_WARNING = "modeWarning";
-
-    // Definitions of the arguments
-    public static final String EXCLUDES_SYMBOLS_ARGS = "excludesSymbolsArgs";
-    public static final String SPECIFY_LENGTH_ARGS = "specifyLengthArgs";
-    public static final String SPECIFY_DELEMITER_ARGS = "specifyDelimiterArgs";
-
-    // Long option names
-    public static final String NO_REPEAT_OPTION = "no-repeat";
-    public static final String NO_UPPER_OPTION = "no-upper";
-    public static final String NO_LOWER_OPTION = "no-lower";
-    public static final String NO_DIGITS_OPTION = "no-digits";
-    public static final String NO_AMBIGUOUS_CHARACTERS_OPTION = "no-ambiguous-characters";
-    public static final String EXCLUDES_ORDER_OPTION = "exclude-order";
-    public static final String EXCLUDES_SYMBOLS_OPTION = "exclude-symbols";
-    public static final String LENGTH_OPTION = "length";
-    public static final String DELIMITER_OPTION = "delimiter";
-    public static final String HELP_OPTION = "help";
-    public static final String PASSWORD_MODE_OPTION = "password-mode";
-
-    // Default values
-    public static final boolean DEFAULT_REPEAT_ALLOWED = true;
-    public static final char DEFAULT_DELIMITER = ' ';
-    public static final int DEFAULT_PASSPHRASE_LENGTH = 6;
-    public static final int DEFAULT_PASSWORD_LENGTH = 12;
-    public static final boolean DEFAULT_ORDER_CONFIGURATION = false;
-    public static final boolean DEFAULT_EXCLUDES_UPPERCASE = false;
-    public static final boolean DEFAULT_EXCLUDES_LOWERCASE = false;
-    public static final boolean DEFAULT_EXCLUDES_DIGITS = false;
-    public static final boolean DEFAULT_EXCLUDES_SYMBOLS = false;
-    public static final boolean DEFAULT_EXCLUDES_AMBIGUOUS = false;
+  // Definitions of the Exclusion string variables
+  public static final String EXCLUDES_REPEAT = "excludesRepeat";
+  public static final String EXCLUDES_UPPERCASE = "excludesUppercase";
+  public static final String EXCLUDES_LOWERCASE = "excludesLowercase";
+  public static final String EXCLUDES_DIGITS = "excludesDigits";
+  public static final String EXCLUDES_SYMBOLS = "excludesSymbols";
+  public static final String EXCLUDES_AMBIGUOUS = "excludesAmbiguous";
+  public static final String EXCLUDES_ORDER = "excludesOrder";
 
 
-    // Character list
-    public static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static final String LOWERCASE = UPPERCASE.toLowerCase();
-    public static final String DIGITS = "0123456789";
-    public static final String SYMBOLS = "*!<>?/@#$%^&()-+_,.\\|{}[]~`:;\'\"";
-    public static final String AMBIGUOUS = "l, 1, o, 0";
+  // Definitions of the Specifications string variables
+  public static final String SPECIFY_LENGTH = "specifyLength";
+  public static final String SPECIFY_DELIMITER = "specifyDelimiter";
+  public static final int MINIMUM_PASSWORD_LENGTH = 5;
+  public static final int MAXIMUM_PASSWORD_LENGTH = 20;
+  public static final int MINIMUM_PASSPHRASE_LENGTH = 7;
+  public static final int MAXIMUM_PASSPHRASE_LENGTH = 12;
+  public static final char[] DELIMITER_SELECTION = {'_','-','*'};
 
-    //TODO add public static final String SYMBOLS
+  // Definition of the mode switch string
+  public static final String MODE_SWITCH = "modeSwitch";
+
+
+  // Definition of the Help Message string
+  public static final String HELP_MSG = "helpMsg";
+
+
+  // Definitions of the Error Messages
+  public static final String MODE_SELECT_ERROR = "modeSelectError";
+  public static final String LENGTH_ERROR = "lengthError";
+  public static final String ARGUMENT_ERROR = "argumentError";
+  public static final String DELIMITER_ERROR = "delimiterErrorMessage";
+
+
+  // Definitions of the Warning Messages
+  public static final String LENGTH_WARNING = "lengthWarning";
+  public static final String MODE_WARNING = "modeWarning";
+  public static final String UNRECOGNIZED_OPTION = "unrecognizedOption";
+  public static final String MISSING_OPTION = "missingOption";
+
+
+  // Definitions of the arguments
+  public static final String EXCLUDES_SYMBOLS_ARGS = "excludesSymbolsArgs";
+  public static final String SPECIFY_LENGTH_ARGS = "specifyLengthArgs";
+  public static final String SPECIFY_DELEMITER_ARGS = "specifyDelimiterArgs";
+
+
+  // long option names
+  public static final String NO_REPEAT_OPTION = "no-repeat";
+  public static final String NO_UPPER_OPTION = "no-upper";
+  public static final String NO_LOWER_OPTION = "no-lower";
+  public static final String NO_DIGITS_OPTION = "no-digits";
+  public static final String NO_AMBIGUOUS_CHARACTERS_OPTION = "no-ambiguous-characters";
+  public static final String EXCLUDES_ORDER_OPTION = "exclude-order";
+  public static final String EXCLUDES_SYMBOLS_OPTION = "exclude-symbols";
+  public static final String LENGTH_OPTION = "length";
+  public static final String DELIMITER_OPTION = "delimiter";
+  public static final String HELP_OPTION = "help";
+  public static final String PASSWORD_MODE_OPTION = "password-mode";
+
+  // Default values
+  public static final boolean DEFAULT_REPEAT_ALLOWED = true;
+  public static final char DEFAULT_DELIMITER = ' ';
+  public static final int DEFAULT_PASSPHRASE_LENGTH = 6;
+  public static final int DEFAULT_PASSWORD_LENGTH = 12;
+  public static final boolean DEFAULT_ORDER_CONFIGURATION = false;
+  public static final boolean DEFAULT_EXCLUDES_UPPERCASE = false;
+  public static final boolean DEFAULT_EXCLUDES_LOWERCASE = false;
+  public static final boolean DEFAULT_EXCLUDES_DIGITS = false;
+  public static final boolean DEFAULT_EXCLUDES_SYMBOLS = false;
+  public static final boolean DEFAULT_EXCLUDES_AMBIGUOUS = false;
+
+
+  // Character list
+  public static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  public static final String LOWERCASE = UPPERCASE.toLowerCase();
+  public static final String DIGITS = "0123456789";
+  public static final String SYMBOLS = "*!<>?/@#$%^&()-+_,.\\|{}[]~`:;\'\"";
+  public static final String AMBIGUOUS = "l, 1, o, 0";
 }
+
+
